c0fc089c8b4f7400e0ecf86582f742217a5ee42a
Gabriela Barbosa Martins <102072743+gabsbarbosam@users.noreply.github.com>
2023-09-18 12:07:02 -0300

    Add files via upload
    A finalidade deste programa é calcular a média de três notas inseridas pelo usuário e determinar se o aluno foi aprovado ou reprovado com base na média calculada. O programa solicita as três notas, calcula a média e, em seguida, exibe a média e uma mensagem indicando se o aluno foi aprovado (se a média for maior ou igual a 7) ou reprovado (se a média for menor que 7).

---
+++ notas.cbl
@@ -0,0 +1,41 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. NOTAS.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+
+           77 WS-NOTA1         PIC 9(02) VALUE ZEROS.
+           77 WS-NOTA2         PIC 9(02) VALUE ZEROS.
+           77 WS-NOTA3         PIC 9(02) VALUE ZEROS.
+           77 WS-RESUL         PIC 9(02) VALUE ZEROS.
+
+           PROCEDURE DIVISION.
+           MAIN-PROCEDURE.
+           DISPLAY "INSIRA NOTA 1"
+           ACCEPT WS-NOTA1
+
+           DISPLAY "INSIRA NOTA 2"
+           ACCEPT WS-NOTA2
+
+           DISPLAY "INSIRA NOTA 3"
+           ACCEPT WS-NOTA3
+
+           COMPUTE WS-RESUL = (WS-NOTA1 + WS-NOTA2 + WS-NOTA3) / 3
+           DISPLAY "SUA MEDIA E " WS-RESUL
+
+           IF WS-RESUL >7 OR = 7
+               THEN
+               DISPLAY "APROVADO"
+           ELSE IF WS-RESUL <7
+               THEN
+               DISPLAY "REPROVADO"
+           END-IF
+
+       STOP RUN.
+       END PROGRAM NOTAS.
